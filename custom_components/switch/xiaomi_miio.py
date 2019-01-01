7222d92dcc8a060a04e075a5727656cfc6e7dc78
Sebastian Muszynski <basti@linkt.de>
2019-01-01 19:58:04 +0100

    Add lumi.acpartner.v3 socket support

--- custom_components/switch/xiaomi_miio.py
+++ custom_components/switch/xiaomi_miio.py
@@ -17,7 +17,7 @@ from homeassistant.const import (
 from homeassistant.exceptions import PlatformNotReady
 import homeassistant.helpers.config_validation as cv
 
-REQUIREMENTS = ['python-miio==0.4.4', 'construct==2.9.45']
+REQUIREMENTS = ['python-miio>=0.4.4', 'construct==2.9.45']
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -40,6 +40,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
          'chuangmi.plug.v2',
          'chuangmi.plug.v3',
          'chuangmi.plug.hmi205',
+         'lumi.acpartner.v3',
          ]),
 })
 
@@ -152,6 +153,12 @@ async def async_setup_platform(hass, config, async_add_entities,
         from miio import ChuangmiPlug
         plug = ChuangmiPlug(host, token, model=model)
         device = XiaomiPlugGenericSwitch(name, plug, model, unique_id)
+        devices.append(device)
+        hass.data[DATA_KEY][host] = device
+    elif model in ['lumi.acpartner.v3']:
+        from miio import AirConditioningCompanion
+        plug = AirConditioningCompanion(host, token, model=model)
+        device = XiaomiAirConditioningCompanionSwitch(name, plug, model, unique_id)
         devices.append(device)
         hass.data[DATA_KEY][host] = device
     else:
@@ -484,6 +491,60 @@ class ChuangMiPlugSwitch(XiaomiPlugGenericSwitch):
 
             if self._channel_usb is False and state.load_power:
                 self._state_attrs[ATTR_LOAD_POWER] = state.load_power
+
+        except DeviceException as ex:
+            self._available = False
+            _LOGGER.error("Got exception while fetching the state: %s", ex)
+
+
+class XiaomiAirConditioningCompanionSwitch(XiaomiPlugGenericSwitch):
+    """Representation of a Xiaomi AirConditioning Companion."""
+
+    def __init__(self, name, plug, model, unique_id):
+        """Initialize the acpartner switch."""
+        super().__init__(name, plug, model, unique_id)
+
+        self._state_attrs[ATTR_TEMPERATURE] = None
+        self._state_attrs.update({
+            ATTR_LOAD_POWER: None,
+        })
+
+    async def async_turn_on(self, **kwargs):
+        """Turn the socket on."""
+        result = await self._try_command(
+            "Turning the socket on failed.", self._plug.socket_on)
+
+        if result:
+            self._state = True
+            self._skip_update = True
+
+    async def async_turn_off(self, **kwargs):
+        """Turn the socket off."""
+        result = await self._try_command(
+            "Turning the socket off failed.", self._plug.socket_off)
+
+        if result:
+            self._state = False
+            self._skip_update = True
+
+    async def async_update(self):
+        """Fetch state from the device."""
+        from miio import DeviceException
+
+        # On state change the device doesn't provide the new state immediately.
+        if self._skip_update:
+            self._skip_update = False
+            return
+
+        try:
+            state = await self.hass.async_add_executor_job(self._plug.status)
+            _LOGGER.debug("Got new state: %s", state)
+
+            self._available = True
+            self._state = state.power_socket == 'on'
+            self._state_attrs.update({
+                ATTR_LOAD_POWER: state.load_power,
+            })
 
         except DeviceException as ex:
             self._available = False
